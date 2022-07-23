0a73a1ff3f3ea4f736ee8a029abdad81f3127bd1
jamiehun <jamiesunghun@gmail.com>
2022-07-23 22:24:07 +0900

    chapter9_최단경로(RV)

--- 2. 이코테_주요알고리즘/최단경로/p259_미래도시.py
+++ 2. 이코테_주요알고리즘/최단경로/p259_미래도시.py
@@ -1,39 +1,36 @@
-""" 제대로 못풀었음 """
-import sys
-import heapq
-
-# N (회사의 개수), M (경로의 개수) 입력
+# 회사의 개수(n)와 간선의 개수(m) 입력
 n, m = map(int, input().split())
+INF = int(1e9) # INF 입력
 
-# Input 입력
-input = sys.stdin.readline
+# 그래프 생성
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
-# graph 생성
-graph = [[] for _ in range(n+1)]
-
-# distance (최소 거리) 입력
-distance = [INF] * (n + 1)
-
-# for 문으로 연결되어 있는 회사 별로 거리 입력
-for i in range(n + 1):
+# 이어져 있는 회사 표시
+for i in range(m):
     a, b = map(int, input().split())
     graph[a][b] = 1
+    graph[b][a] = 1
 
-# 다익스트라 알고리즘 수행
-def dijkstra(start):
-    q = []
-    heapq.push(q, (0, start))
-    distance[start] = 0
-    while q:
-        dist, now = heapq.pop()
-        if distance[now] < dist:
-            continue
-        # 인접한 노드들의 연결에 대한 확인
-        else:
-            for i in len(graph[a]):
-                cost = dist + graph[now][i]
+# 연결된 두 회사의 번호 공백으로 입력
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
 
+# 플로이드 워셜 알고리즘 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
+# x(물건 판매)와 k(소개팅 장소)를 순서대로 입력
+x, k = map(int, input().split())
 
-    
+print(graph)
+
+distance = graph[1][k] + graph[k][x]
+if distance >= INF:
+    print(-1)
+else:
+    print(distance)
 
